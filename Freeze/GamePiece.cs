4ce3e5f3a4022d674bbc1989e467605bf5c91df2
rabell66 <raabell66@gmail.com>
2017-08-13 11:06:21 -0500

    final

---
+++ Freeze/GamePiece.cs
@@ -0,0 +1,74 @@
+﻿using System;
+namespace Freeze
+{
+    public class GamePiece
+    {
+        private int postionX;
+        private int postionY;
+        private bool frozen;
+        private string color = "White";
+        private string name = "Queen";
+
+        public void SetpositionX(int X)
+        {
+            this.postionX = X;
+        }
+        public void SetpositionY(int Y)
+        {
+            this.postionY = Y;
+        }
+        public void Setfrozen(bool frozen)
+        {
+            this.frozen = frozen;
+        }
+        public int GetpositionX()
+        {
+            return this.postionX;
+        }
+        public int GetpositionY()
+        {
+            return this.postionY;
+        }
+        public bool Getfrozen()
+        {
+            return this.frozen;
+        }
+        public string Getname()
+        {
+            return this.name;
+        }
+        public string Getcolor()
+        {
+            return this.color;
+        }
+
+        public GamePiece()
+        {
+            postionX = 0;
+            postionY = 0;
+            frozen = false;
+
+        }
+
+        public void move(int positionX, int positionY)
+        {
+            if (this.frozen == false)
+            {
+                this.postionX = positionX;
+                this.postionY = positionY;
+            }
+            else
+            {
+                return;
+            }
+        }
+        public void freeze()
+        {
+            this.frozen = true;
+        }
+        public void unfreeze()
+        {
+            this.frozen = false;
+        }
+    }
+}
---
+++ Freeze/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Freeze
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            GamePiece gamePiece = new GamePiece();
+            Console.WriteLine(gamePiece.Getcolor());
+            Console.WriteLine(gamePiece.Getname());
+            moveTest(gamePiece);
+            gamePiece.move(1, 4);
+            moveTest(gamePiece);
+            //move frozen
+            gamePiece.freeze();
+            gamePiece.move(2, -3);
+            moveTest(gamePiece);
+            gamePiece.unfreeze();
+            gamePiece.move(2, -3);
+            moveTest(gamePiece);
+
+            //random numbers
+            gamePiece.unfreeze();
+
+            gamePiece.move(randomNum(), randomNum());
+            moveTest(gamePiece);
+
+        }
+
+
+
+        public static void moveTest(GamePiece gamePiece)
+        {
+
+            Console.WriteLine("Frozen: " + gamePiece.Getfrozen());
+            Console.WriteLine("x:" + gamePiece.GetpositionX() + "y:" + gamePiece.GetpositionY());
+
+
+        }
+
+        public static int randomNum()
+        {
+            Random random = new Random();
+            int ran = random.Next(1, 11);
+            return ran;
+        }
+    }
+
+
+
+}
+
+
